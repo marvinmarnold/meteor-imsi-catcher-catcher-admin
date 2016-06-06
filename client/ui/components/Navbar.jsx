9af35ac0149f38a85556fcaab550e939a34642d6
Marvin Arnold <marvinmarnold@gmail.com>
2016-06-06 14:14:45 -0500

    Use absolute paths for static images

--- client/ui/components/Navbar.jsx
+++ client/ui/components/Navbar.jsx
@@ -6,19 +6,27 @@ import { ADMIN_PATHS, adminPathFor } from '../../lib/admin-paths.jsx';
 export default class Navbar extends React.Component {
   render() {
     return (
-      <ul>
-        <li>
-          <Link to={"/" + ADMIN_PATHS.HOME}>Admin Home</Link>
-        </li>
-        <li>
-          <Link to={adminPathFor(ADMIN_PATHS.SIM_READINGS)}>SIM Readings</Link></li>
-        <li>
-          <Link to={adminPathFor(ADMIN_PATHS.TELEPHONY_READINGS)}>Telephony Readings</Link>
-        </li>
-        <li>
-          <Link to={adminPathFor(ADMIN_PATHS.NEIGHBOR_READINGS)}>Neighbor Readings</Link>
-        </li>
-      </ul>
+      <nav className="navbar navbar-light bg-faded">
+        <Link to={"/" + ADMIN_PATHS.HOME}>Admin Home</Link>
+
+        <ul className="nav navbar-nav">
+          <li className="nav-item active">
+            <Link className='nav-link' to={adminPathFor(ADMIN_PATHS.SIM_READINGS)}>
+              SIM Readings
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className='nav-link' to={adminPathFor(ADMIN_PATHS.TELEPHONY_READINGS)}>
+              Telephony Readings
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className='nav-link' to={adminPathFor(ADMIN_PATHS.NEIGHBOR_READINGS)}>
+              Neighbor Readings
+            </Link>
+          </li>
+        </ul>
+      </nav>
     )
   }
 }
--- client/ui/pages/AdminPage.jsx
+++ client/ui/pages/AdminPage.jsx
@@ -3,8 +3,14 @@ import React from 'react';
 export default class AdminPage extends React.Component {
   render() {
     return (
-      <div>
+      <div className='container-fluid'>
         <h1>Admin Page</h1>
+        Use the tabs above to navigate collected data sets.
+        In order to view data, you will need to attach the secret key to the URL, like:
+        <br/>
+        <code>https://stingraymappingproject.org/admin/sim-readings?secret=SECRET_KEY_HERE</code>
+        <br />
+        The password will be saved to the browsing session as long as you don't refresh the page.
       </div>
     )
   }
